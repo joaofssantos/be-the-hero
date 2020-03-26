7fa0e05af8de999a07799268070e8083368d5fa9
João Silva Santos <joao.santos@agenciaeconverse.com.br>
2020-03-25 23:09:53 -0300

    feat: init backend

---
+++ backend/src/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/*
+Rota / Recursos 
+
+*/
+
+/*
+Metodos http
+
+
+GET : Captura informações
+
+POST: Criar informações
+
+PUT: Alterar Informações
+
+DELETE :  Deletar Informações
+*/
+
+/*
+Tipos de Parametro
+
+Query          : parametros nomeados enviados na rota apos '?' (Filtros, paginação)
+Route          : Parametros utilizados para identificar recursos
+Request Body   : Corpo da requisição, utilizado para criar ou alterar recursos
+
+
+BD---
+MY SQL
+SQL LITE
+MS SQL SERVER
+POSTGREE
+ORACLE 
+
+NOSQL
+
+MongoDB
+CouchDB
+
+DRIVER- SELECT * FROM users
+Query Builde : table('users').select('*').where()
+
+
+
+
+*/
+
+
+
+
+
